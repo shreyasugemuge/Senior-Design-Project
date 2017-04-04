f49140dde87b1b1941a2d0aae5e1c8127e02b74c
houshiru <shou2015@my.fit.edu>
2017-04-04 10:12:24 -0400

    Behavior of submit ASR

---
+++ src/Behaviors/Submit_ASR_Date/Beh_Unit1_ASR.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo -e "Caculate how many days of each student submits the ASR after the Unit1 study guide materials avaliable.\n..............\n..............\n"
+python Unit1_ASR.py private/clean/log_*.csv > docs/unit1.csv
+echo -e "Sucess"
