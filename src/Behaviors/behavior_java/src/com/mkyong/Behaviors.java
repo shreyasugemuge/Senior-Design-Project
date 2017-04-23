3c41bb0db9eb114418b307ac40671b5e5d03d512
mohammed <malhabsi2013@my.fit.edu>
2017-04-23 18:12:51 -0400

    added weekly parameter

--- src/Behaviors/behavior_java/src/com/mkyong/Behaviors.java
+++ src/Behaviors/behavior_java/src/com/mkyong/Behaviors.java
@@ -7,17 +7,32 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Scanner;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 
 public class Behaviors {
+	
+    public static Date START_DATE = new Date();
+    public final static SimpleDateFormat FORMAT_DATE = new SimpleDateFormat("M/d/yyyy");
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws ParseException {
+
+    	System.out.println("Enter the number of weeks you want to extract data for:");
+    	Scanner kb = new Scanner(System.in);
+    	int weeks = kb.nextInt();
+    	if (weeks < 0 || weeks > 52) {
+    		System.out.println("Invalid number of weeks. Please choose between 0 and 52");
+    		System.exit(0);
+    	}
+
+    	Date START_DATE = FORMAT_DATE.parse("1/15/2013");
 
         try {
-            CSVWriter csvWriter = new CSVWriter(new FileWriter("OnlineMeetings.csv"));
+            CSVWriter csvWriter = new CSVWriter(new FileWriter("OnlineMeetings(" + weeks + "weeks).csv"));
 
             String[] row = new String[] { "ID", "Online meetings attended" };
             csvWriter.writeNext(row);
@@ -25,7 +40,7 @@ public class Behaviors {
             String[] strings = new String[2];
             for (int i = 0; i < 110; i++) {
                 strings[0] = Integer.toString(i + 1);
-                strings[1] = Integer.toString(onlineMeetings("../private/clean/log_" + (i + 1) + ".csv"));
+                strings[1] = Integer.toString(onlineMeetings("../private/clean/log_" + (i + 1) + ".csv", weeks));
                 csvWriter.writeNext(strings);
             }
             csvWriter.close();
@@ -34,7 +49,7 @@ public class Behaviors {
         }
 
         try {
-            CSVWriter csvWriter = new CSVWriter(new FileWriter("testAverageAct.csv"));
+            CSVWriter csvWriter = new CSVWriter(new FileWriter("testAverageAct(" + weeks + "weeks).csv"));
 
             String[] row = new String[] { "ID", "Average number of activites accessed before each test" };
             csvWriter.writeNext(row);
@@ -42,7 +57,7 @@ public class Behaviors {
             String[] strings = new String[2];
             for (int i = 0; i < 110; i++) {
                 strings[0] = Integer.toString(i + 1);
-                strings[1] = Double.toString(averageAct("../private/clean/log_" + (i + 1) + ".csv"));
+                strings[1] = Double.toString(averageAct("../private/clean/log_" + (i + 1) + ".csv", weeks));
                 csvWriter.writeNext(strings);
             }
             csvWriter.close();
@@ -51,7 +66,7 @@ public class Behaviors {
         }
 
         try {
-            CSVWriter csvWriter = new CSVWriter(new FileWriter("bonusActivities.csv"));
+            CSVWriter csvWriter = new CSVWriter(new FileWriter("bonusActivities(" + weeks + "weeks).csv"));
 
             String[] row = new String[] { "ID", "Procedures Quiz", "Surveys", "CyberRat Assignments" };
             csvWriter.writeNext(row);
@@ -59,7 +74,7 @@ public class Behaviors {
             String[] strings = new String[4];
             for (int i = 0; i < 110; i++) {
 
-                int[] data = bonusActivities("../private/clean/log_" + (i + 1) + ".csv");
+                int[] data = bonusActivities("../private/clean/log_" + (i + 1) + ".csv", weeks);
                 strings[0] = Integer.toString(i + 1);
                 strings[1] = Integer.toString(data[0]);
                 strings[2] = Integer.toString(data[1]);
@@ -73,7 +88,7 @@ public class Behaviors {
         
         try
 		{
-    		CSVWriter csvWriter = new CSVWriter(new FileWriter("testsTakenDates.csv"));
+    		CSVWriter csvWriter = new CSVWriter(new FileWriter("testsTakenDates(" + weeks + "weeks).csv"));
 
     		String[] row = new String[]{"ID","days between test 1 available and submission", "days between test 2 available and submission"
     				, "days between test 3 available and submission", "days between test 4 available and submission"
@@ -84,13 +99,34 @@ public class Behaviors {
 			String[] strings = new String[8];
 			for (int i = 0; i < 110; i++) {
 	              strings[0] = Integer.toString(i+1);
-	              strings[1] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 1, 25));
-	              strings[2] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 2, 1));
-	              strings[3] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 3, 15));
-	              strings[4] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 4, 1));
-	              strings[5] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 5, 8));
-	              strings[6] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 6, 15));
-	              strings[7] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 7, 22));
+	              strings[1] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 1, 25, weeks));
+	              if (strings[1].equals("-1")) {
+	            	  strings[1] = "-";
+	              }
+	              strings[2] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 2, 1, weeks));
+	              if (strings[2].equals("-1")) {
+	            	  strings[2] = "-";
+	              }
+	              strings[3] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 3, 15, weeks));
+	              if (strings[3].equals("-1")) {
+	            	  strings[3] = "-";
+	              }
+	              strings[4] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 4, 1, weeks));
+	              if (strings[4].equals("-1")) {
+	            	  strings[4] = "-";
+	              }
+	              strings[5] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 5, 8, weeks));
+	              if (strings[5].equals("-1")) {
+	            	  strings[5] = "-";
+	              }
+	              strings[6] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 6, 15, weeks));
+	              if (strings[6].equals("-1")) {
+	            	  strings[6] = "-";
+	              }
+	              strings[7] = Integer.toString(testDateTimely("../private/clean/log_" + (i+1) + ".csv", 7, 22, weeks));
+	              if (strings[7].equals("-1")) {
+	            	  strings[7] = "-";
+	              }
 	              csvWriter.writeNext(strings);
 	        }
             csvWriter.close();
@@ -102,7 +138,7 @@ public class Behaviors {
         
     }
 
-    public static int onlineMeetings (String FILE_NAME) {
+    public static int onlineMeetings (String FILE_NAME, int weeks) {
 
         int numOnline = 0;
         try {
@@ -115,6 +151,12 @@ public class Behaviors {
 
             while ((nextLine = reader.readNext()) != null) {
 
+            	String curDate;
+                if ((curDate = nextLine[1]).equals(lastDate(weeks))) {
+                    reader.close();
+                    return numOnline;
+                }
+            	
                 lineNumber++;
                 name1 = nextLine[6].toString();
 
@@ -134,7 +176,7 @@ public class Behaviors {
         return numOnline;
     }
 
-    public static int[] bonusActivities (String FILE_NAME) {
+    public static int[] bonusActivities (String FILE_NAME, int weeks) {
 
         int[] bonus = new int[3];
         int quiz = 0, surveys = 0, CyberRat = 0;
@@ -147,6 +189,15 @@ public class Behaviors {
 
             while ((nextLine = reader.readNext()) != null) {
 
+            	String curDate;
+                if ((curDate = nextLine[1]).equals(lastDate(weeks))) {
+                    reader.close();
+                    bonus[0] = quiz;
+                    bonus[1] = surveys;
+                    bonus[2] = CyberRat;
+                    return bonus;
+                }
+            	
                 lineNumber++;
                 name1 = nextLine[6].toString();
 
@@ -174,7 +225,7 @@ public class Behaviors {
         return bonus;
     }
 
-    public static double averageAct (String FILE_NAME) {
+    public static double averageAct (String FILE_NAME, int weeks) {
         double average = 0;
 
         try {
@@ -191,6 +242,13 @@ public class Behaviors {
 
             while ((nextLine = reader.readNext()) != null) {
 
+            	String curDate;
+                if ((curDate = nextLine[1]).equals(lastDate(weeks))) {
+                    reader.close();
+                    average = (test1Act + test2Act + test3Act + test4Act + test5Act + test6Act + test7Act) / 7;
+                    return average;
+                }
+            	
                 lineNumber++;
                 name1 = nextLine[6].toString();
 
@@ -242,7 +300,7 @@ public class Behaviors {
         return average;
     }
     
-    public static int testDateTimely(String FILE_NAME, int exam,  int examDate) throws ParseException {
+    public static int testDateTimely(String FILE_NAME, int exam,  int examDate, int weeks) throws ParseException {
     	int days = 0;
     	
     	try {
@@ -254,7 +312,13 @@ public class Behaviors {
             
             while ((nextLine = reader.readNext()) != null) {
 
-
+              	String curDate;
+                if ((curDate = nextLine[1]).equals(lastDate(weeks))) {
+                    reader.close();
+                    return -1;
+                }
+            	
+            	
             	lineNumber++;
                 name1 = nextLine[6].toString();
                 
@@ -339,4 +403,11 @@ public class Behaviors {
     	return days;
     } 
 
+    public static String lastDate (int weeks) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(START_DATE);
+        c.add(Calendar.DATE, (weeks * 7));
+        return FORMAT_DATE.format(c.getTime());
+
+    }
 }
