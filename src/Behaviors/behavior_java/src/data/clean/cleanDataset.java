921ee7f0b14107bbed4739498dd00b50822a2dd7
shreyas ugemuge <shreyas@ugemuge.com>
2017-04-24 04:22:11 -0400

    fixed clean dataset and ported to java. Some error handling

--- src/Behaviors/behavior_java/src/data/clean/cleanDataset.java
+++ src/Behaviors/behavior_java/src/data/clean/cleanDataset.java
@@ -12,32 +12,38 @@ import com.opencsv.CSVWriter;
 public class cleanDataset {
     final static String ALLLOGS_REGEX = "^Learner_Profile_Activity_Logs \\(\\d|\\d\\d|\\d\\d\\d\\)\\.csv$";
     final static File DIR_ALLLOGS = new File("../private/alllogs/");
-    final static File DIR_CLEAN = new File("../private/clan/");
+    final static File DIR_CLEAN = new File("../private/clean/");
 
     public static void main (String[] args) throws Exception { // bad error
                                                                // handling but
                                                                // meh
         String[] cur;
-        int logCount = 1;
         System.out.print(DIR_CLEAN.mkdir() ? "Creating new folder:" + DIR_CLEAN.getPath() + '\n' : "");
         File[] files = getAllMatchingFromDir(DIR_ALLLOGS, ALLLOGS_REGEX);
         for (File f : files) {
             CSVReader in = new CSVReader(new FileReader(f));
-            CSVWriter out = new CSVWriter(new FileWriter(DIR_CLEAN.getName() + "log_" + logCount++ + ".csv"));
+            CSVWriter out = new CSVWriter(
+                    new FileWriter("../private/clean/log_" + f.getName().split("\\(|\\)")[1] + ".csv"));
             out.writeNext(handleTitles(in.readNext()));
-            while ((cur = in.readNext()) != null)
+            while ((cur = in.readNext()) != null && cur[0] != null && !cur[0].contains("4/13/2013"))
                 out.writeNext(cleanRow(cur));
+            // System.out.println(logCount + " " + cur[1]);
             in.close();
+            out.flush();
             out.close();
         }
     }
 
     private static String[] cleanRow (String[] cur) {
-        System.out.println(cur.length);
         if (cur.length >= 6 && cur[5] == cur[6])
             cur[6] = "";
 
-        return cur;
+        String[] cur2 = new String[cur.length + 1];
+        cur2[0] = "";
+        for (int i = 1; i < cur2.length; i++) {
+            cur2[i] = cur[i - 1];
+        }
+        return cur2;
     }
 
     private static String[] handleTitles (String[] titles) {
