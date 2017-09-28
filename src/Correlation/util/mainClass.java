5a4c9fa9b56f7197176d4ae433e91a5c4c3aa236
shreyas ugemuge <shreyas@ugemuge.com>
2017-09-28 08:57:43 -0400

    minor fix and major cleanup

--- src/Correlation/util/mainClass.java
+++ src/Correlation/util/mainClass.java
@@ -1,3 +1,5 @@
+package corr;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -10,18 +12,17 @@ public class mainClass {
     public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(new File("data/merge.csv")));
         switch (Integer.parseInt(args[1])) {
-            case 2:
-                toTrain2(br, Integer.parseInt(args[0]));
-                break;
-            case 1:
-                toTrain(br, Integer.parseInt(args[0]));
-                break;
-            default:
-                System.err.println("argument error");
-                System.exit(1);
+        case 2:
+            // toTrain2(br, Integer.parseInt(args[0]));
+            break;
+        case 1:
+            toTrain(br, Integer.parseInt(args[0]));
+            break;
+        default:
+            System.err.println("argument error");
+            System.exit(1);
         }
     }
-
 
     private static void toTrain (BufferedReader br, int i) throws IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter(new File("data/train.csv")));
@@ -30,14 +31,14 @@ public class mainClass {
         String temp = "";
         String fl[] = br.readLine().split(",");
         String first = "";
-        while(int i =0 ; i < fl.length;i++)
-          first += fl[i] + (i == fl.length-1 ? "\n" : ",")
+        for (int k = 0; k < fl.length; k++)
+            first += fl[k] + (k == fl.length - 1 ? "\n" : ",");
         bw.write(first);
         bw.flush();
         while (i-- > 1 && (row = br.readLine()) != null) {
             col = row.split(",");
-            while(int i =0 ; i < col.length;i++)
-              temp += col[i] + (i == col.length-1 ? "\n" : ",")
+            for (int k = 0; k < col.length; k++)
+                temp += col[k] + (k == col.length - 1 ? "\n" : ",");
             bw.write(temp);
             bw.flush();
         }
@@ -47,58 +48,13 @@ public class mainClass {
         bw.flush();
         while ((row = br.readLine()) != null) {
             col = row.split(",");
-            while(int i =0 ; i < col.length;i++)
-              temp += col[i] + (i == col.length-1 ? "\n" : ",")
+            for (int k = 0; k < col.length; k++)
+                temp += col[k] + (k == col.length - 1 ? "\n" : ",");
             bw.write(temp);
             bw.flush();
         }
         bw.close();
 
     }
-
-    // private static void toTrain2 (BufferedReader br, int i) throws IOException {
-    //     BufferedWriter bw = new BufferedWriter(new FileWriter(new File("data/train.csv")));
-    //     BufferedWriter bw2 = new BufferedWriter(new FileWriter(new File("data/test.csv")));
-    //     int ys = 0, ns = 0;
-    //     String row;
-    //     String[] col;
-    //     String fl[] = br.readLine().split(",");
-    //     String first = fl[0] + "," + fl[1] + "," + fl[2] + "," + fl[3] + "," + fl[4] + "," + fl[5] + ","+  fl[fl.length - 1] + "\n";
-    //     bw.write(first);
-    //     bw2.write(first);
-    //     while ((row = br.readLine()) != null) {
-    //         col = row.split(",");
-    //         if (col[col.length - 1].toString().equals("Y")) {
-    //             ys++;
-    //             if (ys > i / 2) {
-    //                 bw2.write(col[0] + "," + col[1] + "," + col[2] + "," + col[3] + "," + col[4] + "," + col[5] + "," + col[col.length - 1] + "\n");
-    //                 bw2.flush();
-    //                 continue;
-    //             } else {
-    //                 bw.write(col[0] + "," + col[1] + "," + col[2] + "," + col[3] + "," + col[4] + "," + col[5] + "," + col[col.length - 1] + "\n");
-    //                 bw.flush();
-    //                 continue;
-    //             }
-    //         } else {
-    //             ns++;
-    //             if (ns > i / 2) {
-    //                 bw2.write(col[0] + "," + col[1] + "," + col[2] + "," + col[3] + "," + col[4] + "," + col[5] + "," + col[col.length - 1] + "\n");
-    //                 bw2.flush();
-    //                 continue;
-    //             } else {
-    //                 bw.write(col[0] + "," + col[1] + "," + col[2] + "," + col[3] + "," + col[4] + "," + col[5] + "," + col[col.length - 1] + "\n");
-    //                 bw.flush();
-    //                 continue;
-    //             }
-    //         }
-    //     }
-    //     while ((row = br.readLine()) != null) {
-    //         col = row.split(",");
-    //         bw2.write(col[0] + "," + col[1] + "," + col[2] + "," + col[3] + "," + col[4] + "," + col[5] + "," + col[col.length - 1] + "\n");
-    //     }
-    //     System.out.println("n: " + ns + " y: " + ys);
-    //     bw.close();
-    //     bw2.close();
-    // }
 
 }
